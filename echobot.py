500406db3a889fd72be3481a5863149e75785506
DoctorPink <65646938+DoctorPink@users.noreply.github.com>
2025-08-01 15:01:05 -0400

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -22,17 +22,18 @@ if "messages" not in st.session_state:
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+        st.markdown(message["content"])         # response typed into chat message
  
 # React to user input
 if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
-    st.chat_message("user").markdown(prompt)
+    st.chat_message("user").markdown(prompt)     
 
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    response = f"Echo: {prompt}"
+    # Add response to display, the assistant echo users into chat message
+    response = f"Echo: {prompt}"              
     # Display assistant response in chat message container
     
     with st.chat_message("assistant"):
