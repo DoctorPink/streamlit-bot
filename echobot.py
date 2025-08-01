954c7943aba52918ecb1dbcf17898963a743ecd1
DoctorPink <65646938+DoctorPink@users.noreply.github.com>
2025-08-01 14:48:40 -0400

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,10 +1,15 @@
 import streamlit as st
 import numpy as np
 
-#with st.chat_message("user"):  # should be from the computer side
+# with st.chat_message("user"):  # should be from the person or user side
 #    st.write("Hello 👋") 
  
-with st.chat_message("assistant"):
-    st.write("Hello human")
-    st.bar_chart(np.random.randn(30, 3))
+# with st.chat_message("assistant"): # should be from the computer side
+ #   st.write("Hello human")
+ #   st.bar_chart(np.random.randn(30, 3))  # display a bar chart with random data
 
+# String is returned
+
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
