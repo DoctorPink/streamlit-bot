242cdd9bbc206eec1912ab831b270b8571c04ef5
DoctorPink <65646938+DoctorPink@users.noreply.github.com>
2025-08-01 14:56:05 -0400

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -5,11 +5,26 @@ import numpy as np
 #    st.write("Hello 👋") 
  
 # with st.chat_message("assistant"): # should be from the computer side
- #   st.write("Hello human")
- #   st.bar_chart(np.random.randn(30, 3))  # display a bar chart with random data
+#   st.write("Hello human")
+#   st.bar_chart(np.random.randn(30, 3))  # display a bar chart with random data
 
 # String is returned
+# prompt = st.chat_input("Say something")
+# if prompt:
+#    st.write(f"User has sent the following prompt: {prompt}")
 
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
+st.title("Echo Bot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+
+
+
